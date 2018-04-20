98f2c962aea427f3f98be2859eaf83ecb7149dcb
nguyenthanhlinh <thanhlinh12314@gmail.com>
2018-04-20 09:00:57 +0700

    implement interface resizeable

--- CircleTest.java
+++ CircleTest.java
@@ -1,12 +1,19 @@
 public class CircleTest {
     public static void main(String[] args) {
-        Circle circle = new Circle();
-        System.out.println(circle);
+        Circle[] circle = new Circle[3];
+        circle[0] = new Circle();
+        circle[1] = new Circle(4);
+        circle[2] = new Circle("red", false, 5);
 
-        circle = new Circle("white", false, 3);
-        System.out.println(circle);
+        for (Circle c : circle) {
+            System.out.println(c);
+        }
 
-        circle.resize(0.5);
-        System.out.println(circle);
+        double percent = Math.random()*1;
+        System.out.println(percent);
+        for (int i = 0; i < circle.length; i++) {
+            circle[i].resize(percent);
+            System.out.println(circle[i]);
+        }
     }
 }
